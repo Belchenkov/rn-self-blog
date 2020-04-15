9d2ce360728886f4803ef3106e69306965435546
Aleksey Belchenkov <u608110@gmail.com>
2020-04-15 14:55:46 +0300

    Navigation Styles

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import MainScreen from "../screens/MainScreen";
 import PostScreen from "../screens/PostScreen";
+import { THEME } from "../theme";
 
 const PostNavigator = createStackNavigator({
     Main: MainScreen,
@@ -10,7 +11,13 @@ const PostNavigator = createStackNavigator({
         screen: PostScreen
     }
 }, {
-    initialRouteName: 'Main'
+    initialRouteName: 'Main',
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: THEME.MAIN_COLOR
+        },
+        headerTintColor: '#fff'
+}
 });
 
 export const AppNavigation = createAppContainer(PostNavigator);
---
+++ src/theme.js
@@ -0,0 +1,4 @@
+export const THEME = {
+    MAIN_COLOR: '#075a13',
+    DANGER_COLOR: '#D81B60'
+};
